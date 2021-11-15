a09e68c66cafd4356a38e86c1c10677ede4eda34
ceronchaparrocarlosandres <ceronchaparrocarlosandres@gmail.com>
2021-11-15 12:08:17 -0500

    Merge remote-tracking branch 'github/master'

--- app/src/main/java/com/example/sucursalesapp/AcercaDe.java
+++ app/src/main/java/com/example/sucursalesapp/AcercaDe.java
@@ -3,6 +3,8 @@ package com.example.sucursalesapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AcercaDe extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class AcercaDe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acerca_de);
+
+        Button btnvolver = (Button) findViewById(R.id.button5);
+        btnvolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
